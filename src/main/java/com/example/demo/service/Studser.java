3d316356e75668c8b1666e395fe6b54aa9a197cf
bala-amypo <bala@amypo.in>
2025-12-17 05:03:39 +0000

    User commit

--- src/main/java/com/example/demo/service/Studser.java
+++ src/main/java/com/example/demo/service/Studser.java
@@ -4,4 +4,19 @@ package com.example.demo.service;
 import com.example.demo.service.StudentService;
 public class StudSer implementation StudentService{
        Map<Integer,StudentEntity> details=new HashMap<>();
-}
+       public StudentEntity saveData(StudentEntity st){
+        details.put(st.getId(),st);
+        return st;
+       }
+       @Override
+       public StudentEntity insertStudent(StudentEntity st){
+        details.put(st.getId(),st);
+        return st;
+       }
+       @Override
+       public List<StudentEntity>getAllStudent(){
+        return new ArrayList<>(details.value());
+       }
+       @Override
+       
+} 
