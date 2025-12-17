aea04a0e1f5d23654a5a2d126986f07cdceac7d5
bala-amypo <bala@amypo.in>
2025-12-17 05:08:39 +0000

    User commit

--- src/main/java/com/example/demo/service/Studser.java
+++ src/main/java/com/example/demo/service/Studser.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
-
+import java.util.*;
+import org.springframeworks.
 import com.example.demo.service.StudentService;
 public class StudSer implementation StudentService{
        Map<Integer,StudentEntity> details=new HashMap<>();
@@ -18,5 +19,11 @@ public class StudSer implementation StudentService{
         return new ArrayList<>(details.value());
        }
        @Override
-       
+       public Optinal<StudentEntity>getOneStudent(){
+        return Optinal.ofNullable(details.get(id));
+       }
+       @Override
+       public void deleteStudent(int id){
+        details.remove(id);
+       }
 } 
